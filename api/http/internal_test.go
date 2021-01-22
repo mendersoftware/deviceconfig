a0f65013e6ca1212401d8b2bd3ab129608654621
Alf-Rune Siqveland <alf.rune@northern.tech>
2021-01-22 15:32:31 +0100

    api/http: Unit tests for provision and decommission device endpoints
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- api/http/internal_test.go
+++ api/http/internal_test.go
@@ -15,19 +15,28 @@
 package http
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/mendersoftware/go-lib-micro/rest.utils"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
 	mapp "github.com/mendersoftware/deviceconfig/app/mocks"
+	"github.com/mendersoftware/deviceconfig/model"
+	"github.com/mendersoftware/deviceconfig/store"
 )
+
+var contextMatcher = mock.MatchedBy(func(v context.Context) bool {
+	return true
+})
 
 func TestAlive(t *testing.T) {
 	t.Parallel()
@@ -91,6 +100,295 @@ func TestHealth(t *testing.T) {
 					string(b),
 					w.Body.String(),
 				)
+			}
+		})
+	}
+}
+
+func TestProvisionDevice(t *testing.T) {
+	t.Parallel()
+	newDeviceMatcher := func(expected uuid.UUID) interface{} {
+		return mock.MatchedBy(func(dev model.NewDevice) bool {
+			return assert.Equal(t, expected, dev.ID)
+		})
+	}
+	testCases := []struct {
+		Name string
+
+		Request *http.Request
+
+		App    *mapp.App
+		Error  *rest.Error
+		Status int
+	}{{
+		Name: "ok",
+
+		Request: func() *http.Request {
+			body, _ := json.Marshal(map[string]interface{}{
+				"device_id": uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				),
+			})
+
+			req, _ := http.NewRequest("POST",
+				"http://localhost"+URIInternal+URITenantDevices,
+				bytes.NewReader(body),
+			)
+			return req
+		}(),
+
+		App: func() *mapp.App {
+			app := new(mapp.App)
+			app.On("ProvisionDevice",
+				contextMatcher,
+				newDeviceMatcher(uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				)),
+			).Return(nil)
+			return app
+		}(),
+		Status: http.StatusCreated,
+	}, {
+		Name: "error bad request body",
+
+		Request: func() *http.Request {
+			req, _ := http.NewRequest("POST",
+				"http://localhost"+URIInternal+URITenantDevices,
+				bytes.NewReader([]byte("device_id=foobar")),
+			)
+			req.Header.Set("X-Men-Requestid", "test")
+			return req
+		}(),
+
+		App: new(mapp.App),
+		Error: &rest.Error{
+			Err: "invalid request body: invalid character " +
+				"'d' looking for beginning of value",
+			RequestID: "test",
+		},
+		Status: http.StatusBadRequest,
+	}, {
+		Name: "error invalid request body",
+
+		Request: func() *http.Request {
+			body, _ := json.Marshal(map[string]interface{}{
+				"user_id": uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				),
+			})
+
+			req, _ := http.NewRequest("POST",
+				"http://localhost"+URIInternal+URITenantDevices,
+				bytes.NewReader(body),
+			)
+			req.Header.Set("X-Men-Requestid", "test")
+			return req
+		}(),
+
+		App: new(mapp.App),
+		Error: &rest.Error{
+			Err:       "invalid request body: device_id: cannot be blank.",
+			RequestID: "test",
+		},
+		Status: http.StatusBadRequest,
+	}, {
+		Name: "error invalid request body",
+
+		Request: func() *http.Request {
+			body, _ := json.Marshal(map[string]interface{}{
+				"device_id": uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				),
+			})
+
+			req, _ := http.NewRequest("POST",
+				"http://localhost"+URIInternal+URITenantDevices,
+				bytes.NewReader(body),
+			)
+			req.Header.Set("X-Men-Requestid", "test")
+			return req
+		}(),
+
+		App: func() *mapp.App {
+			app := new(mapp.App)
+			app.On("ProvisionDevice",
+				contextMatcher,
+				newDeviceMatcher(uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				)),
+			).Return(errors.New("something went wrong!"))
+			return app
+		}(),
+		Error: &rest.Error{
+			Err:       "internal error",
+			RequestID: "test",
+		},
+		Status: http.StatusInternalServerError,
+	}}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			defer tc.App.AssertExpectations(t)
+			router := NewRouter(tc.App)
+
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, tc.Request)
+			assert.Equal(t, tc.Status, w.Code)
+			if tc.Error != nil {
+				b, _ := json.Marshal(tc.Error)
+				assert.JSONEq(t, string(b), string(w.Body.Bytes()))
+			}
+		})
+	}
+}
+
+func TestDecommissionDevice(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		Name string
+
+		Request *http.Request
+
+		App    *mapp.App
+		Error  *rest.Error
+		Status int
+	}{{
+		Name: "ok",
+
+		Request: func() *http.Request {
+			repl := strings.NewReplacer(
+				":tenant_id", "123456789012345678901234",
+				":device_id", uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				).String(),
+			)
+			req, _ := http.NewRequest("DELETE",
+				"http://localhost"+URIInternal+
+					repl.Replace(URITenantDevice),
+				nil,
+			)
+			return req
+		}(),
+
+		App: func() *mapp.App {
+			app := new(mapp.App)
+			app.On("DecommissionDevice",
+				contextMatcher,
+				uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				),
+			).Return(nil)
+			return app
+		}(),
+		Status: http.StatusNoContent,
+	}, {
+		Name: "error device not found",
+
+		Request: func() *http.Request {
+			repl := strings.NewReplacer(
+				":tenant_id", "123456789012345678901234",
+				":device_id", uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				).String(),
+			)
+			req, _ := http.NewRequest("DELETE",
+				"http://localhost"+URIInternal+
+					repl.Replace(URITenantDevice),
+				bytes.NewReader([]byte("device_id=foobar")),
+			)
+			req.Header.Set("X-Men-Requestid", "test")
+			return req
+		}(),
+
+		App: func() *mapp.App {
+			app := new(mapp.App)
+			app.On("DecommissionDevice",
+				contextMatcher,
+				uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				),
+			).Return(errors.Wrap(store.ErrDeviceNoExist, "mongo"))
+			return app
+		}(),
+		Error: &rest.Error{
+			Err:       store.ErrDeviceNoExist.Error(),
+			RequestID: "test",
+		},
+		Status: http.StatusNotFound,
+	}, {
+		Name: "error invalid request parameters",
+
+		Request: func() *http.Request {
+			repl := strings.NewReplacer(
+				":tenant_id", "123456789012345678901234",
+				":device_id", "notUUID",
+			)
+			req, _ := http.NewRequest("DELETE",
+				"http://localhost"+URIInternal+
+					repl.Replace(URITenantDevice),
+				nil,
+			)
+			req.Header.Set("X-Men-Requestid", "test")
+			return req
+		}(),
+
+		App: new(mapp.App),
+		Error: &rest.Error{
+			Err:       "device_id is not a valid UUID.",
+			RequestID: "test",
+		},
+		Status: http.StatusBadRequest,
+	}, {
+		Name: "error, internal server error",
+
+		Request: func() *http.Request {
+			repl := strings.NewReplacer(
+				":tenant_id", "123456789012345678901234",
+				":device_id", uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				).String(),
+			)
+			req, _ := http.NewRequest("DELETE",
+				"http://localhost"+URIInternal+
+					repl.Replace(URITenantDevice),
+				bytes.NewReader([]byte("device_id=foobar")),
+			)
+			req.Header.Set("X-Men-Requestid", "test")
+			return req
+		}(),
+
+		App: func() *mapp.App {
+			app := new(mapp.App)
+			app.On("DecommissionDevice",
+				contextMatcher,
+				uuid.NewSHA1(
+					uuid.NameSpaceDNS, []byte("mender.io"),
+				),
+			).Return(errors.New("Oh noez!"))
+			return app
+		}(),
+		Error: &rest.Error{
+			Err:       "internal error",
+			RequestID: "test",
+		},
+		Status: http.StatusInternalServerError,
+	}}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			defer tc.App.AssertExpectations(t)
+			router := NewRouter(tc.App)
+
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, tc.Request)
+			assert.Equal(t, tc.Status, w.Code)
+			if tc.Error != nil {
+				b, _ := json.Marshal(tc.Error)
+				assert.JSONEq(t, string(b), string(w.Body.Bytes()))
 			}
 		})
 	}
